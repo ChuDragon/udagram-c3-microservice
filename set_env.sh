16bc94b4af6f0eb8a4814962c1dc91c9f82c3018
Roman Chuyan <92877535+ChuDragon@users.noreply.github.com>
2022-01-14 13:24:35 -0500

    Updated env vars, .gitignore

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=rcdatabase
+export POSTGRES_PASSWORD=Chook338
+export POSTGRES_HOST=rcdatabase.csf2sckfllmb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=rcdatabase
+export AWS_BUCKET=arn:aws:s3:::rc-udagram-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=rc-udagram-secret
+export URL=http://localhost:8100
